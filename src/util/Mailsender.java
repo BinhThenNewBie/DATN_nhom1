002bfdee36afa3875c825b2dc21221169a463c1d
Bình_CG <binhdath06811@gmail.com>
2025-07-04 20:59:54 +0700

    mail sender struct

---
+++ src/util/Mailsender.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author PC Của Bình
+ */
+public class Mailsender {
+    // password: sera mtlj hgug ykvk
+    // username: binhdath06811@gmail.com
+}
