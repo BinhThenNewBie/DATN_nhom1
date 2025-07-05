0d990b0a6576bc7d0b9b67b6741257bb9608f2bb
mtam-ai <mtamdtrai@gmail.com>
2025-07-05 15:39:09 +0700

    model + dao

--- src/DAO/UuDaiDAO.java
+++ src/DAO/UuDaiDAO.java
@@ -4,10 +4,91 @@
  */
 package DAO;
 
+import DBconnect.DBconnect;
+import Model.UuDai;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author nmttt
  */
 public class UuDaiDAO {
-    
+     public List<UuDai> getAll() {
+        List<UuDai> list = new ArrayList<>();
+        String sql = "SELECT * FROM UUDAI";
+        Connection con = DBconnect.getConnection();
+        try {
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {
+                UuDai u = new UuDai();
+                u.setIdUD(rs.getString(1));
+                u.setGiaTri(rs.getString(2));
+                u.setSoLuong(rs.getInt(3));
+                u.setNgayBatDau(rs.getDate(4));
+                u.setNgayKetThuc(rs.getDate(5));
+                list.add(u);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+
+    public Object[] getRow(UuDai u) {
+        return new Object[] {
+            u.getIdUD(), u.getGiaTri(), u.getSoLuong(),
+            u.getNgayBatDau(), u.getNgayKetThuc()
+        };
+    }
+
+    public UuDai timKiem(String idUD) {
+        String sql = "SELECT * FROM UUDAI WHERE ID_UD = ?";
+        try (Connection con = DBconnect.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, idUD);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                UuDai u = new UuDai();
+                u.setIdUD(rs.getString(1));
+                u.setGiaTri(rs.getString(2));
+                u.setSoLuong(rs.getInt(3));
+                u.setNgayBatDau(rs.getDate(4));
+                u.setNgayKetThuc(rs.getDate(5));
+                return u;
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public List<UuDai> locTheoGiaTri(String giaTri) {
+        List<UuDai> list = new ArrayList<>();
+        String sql = "SELECT * FROM UUDAI WHERE GIATRI = ?";
+
+        try (Connection conn = DBconnect.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, giaTri);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                UuDai u = new UuDai();
+                u.setIdUD(rs.getString(1));
+                u.setGiaTri(rs.getString(2));
+                u.setSoLuong(rs.getInt(3));
+                u.setNgayBatDau(rs.getDate(4));
+                u.setNgayKetThuc(rs.getDate(5));
+                list.add(u);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
--- src/Model/UuDai.java
+++ src/Model/UuDai.java
@@ -4,10 +4,68 @@
  */
 package Model;
 
+import java.sql.Date;
+
 /**
  *
  * @author nmttt
  */
 public class UuDai {
+    private String idUD;
+    private String giaTri;
+    private int soLuong;
+    private java.sql.Date ngayBatDau;
+    private java.sql.Date ngayKetThuc;
+
+    public UuDai() {
+    }
+
+    public UuDai(String idUD, String giaTri, int soLuong, Date ngayBatDau, Date ngayKetThuc) {
+        this.idUD = idUD;
+        this.giaTri = giaTri;
+        this.soLuong = soLuong;
+        this.ngayBatDau = ngayBatDau;
+        this.ngayKetThuc = ngayKetThuc;
+    }
+
+    public String getIdUD() {
+        return idUD;
+    }
+
+    public void setIdUD(String idUD) {
+        this.idUD = idUD;
+    }
+
+    public String getGiaTri() {
+        return giaTri;
+    }
+
+    public void setGiaTri(String giaTri) {
+        this.giaTri = giaTri;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public Date getNgayBatDau() {
+        return ngayBatDau;
+    }
+
+    public void setNgayBatDau(Date ngayBatDau) {
+        this.ngayBatDau = ngayBatDau;
+    }
+
+    public Date getNgayKetThuc() {
+        return ngayKetThuc;
+    }
+
+    public void setNgayKetThuc(Date ngayKetThuc) {
+        this.ngayKetThuc = ngayKetThuc;
+    }
     
 }
