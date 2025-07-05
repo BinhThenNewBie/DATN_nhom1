8be06bf54b90b21ede30f3e1bce64507834a37b4
mtam-ai <mtamdtrai@gmail.com>
2025-07-05 16:17:32 +0700

    Revert "Merge branch 'main' of https://github.com/BinhThenNewBie/DATN_nhom1"
    This reverts commit 53e3e8552805b1b5c3134060a5bba6fe5b571f5e, reversing
    changes made to 08b7c12b21090ec46f9113b51d354dc0e76c69a0.

--- src/DAO/NhanvienDAO.java
+++ src/DAO/NhanvienDAO.java
@@ -3,36 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-import DBconnect.DBconnect;
-import Model.Nhanvien;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+
 /**
  *
  * @author ADMIN
  */
 public class NhanvienDAO {
-    public List<Nhanvien> GETALL(){
-        List<Nhanvien> Listnv = new ArrayList<>();
-        String sql ="SELECT * FROM NHANVIEN";
-        Connection conn = DBconnect.getConnection();
-        try {
-            Statement stm = conn.createStatement();
-            ResultSet rs = stm.executeQuery(sql);
-            while (rs.next()) {
-                Nhanvien nv = new Nhanvien();
-                nv.setID_NV(rs.getString(1));
-                nv.setHoTen(rs.getNString(2));
-                nv.setSDT(rs.getString(3));
-                nv.setChucVu(rs.getString(4));
-                nv.setIMG(rs.getString(5));
-            }
-        } catch (Exception e) {
-        }
-        return Listnv;
-    }
+    
 }
