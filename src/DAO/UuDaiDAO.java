bc72dd58c9aa47ca85fd3eb679c8271629babfc6
hgiang16 <giangthth005801@gmail.com>
2025-07-05 16:20:38 +0700

    Revert "Merge branch 'main' of https://github.com/BinhThenNewBie/DATN_nhom1"
    This reverts commit 11eaf56b9908b5ba3d69802d1201c253fef29c6e, reversing
    changes made to 82f63a219e3934952ef2fe3411a68d6213b4eb81.

--- src/DAO/UuDaiDAO.java
+++ src/DAO/UuDaiDAO.java
@@ -5,7 +5,6 @@
 package DAO;
 
 import DBconnect.DBconnect;
-import Model.SanPham;
 import Model.UuDai;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,9 +18,8 @@ import java.util.List;
  * @author nmttt
  */
 public class UuDaiDAO {
-
-    public List<UuDai> getAll() {
-        List<UuDai> lstUD = new ArrayList<>();
+     public List<UuDai> getAll() {
+        List<UuDai> list = new ArrayList<>();
         String sql = "SELECT * FROM UUDAI";
         Connection con = DBconnect.getConnection();
         try {
@@ -34,79 +32,63 @@ public class UuDaiDAO {
                 u.setSoLuong(rs.getInt(3));
                 u.setNgayBatDau(rs.getDate(4));
                 u.setNgayKetThuc(rs.getDate(5));
-                lstUD.add(u);
+                list.add(u);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return lstUD;
+        return list;
     }
 
     public Object[] getRow(UuDai u) {
-        return new Object[]{
+        return new Object[] {
             u.getIdUD(), u.getGiaTri(), u.getSoLuong(),
             u.getNgayBatDau(), u.getNgayKetThuc()
         };
     }
 
-    public void them(UuDai u) {
-        String sql = "INSERT INTO UUDAI (ID_UD, GIATRI, SOLUONG, NGAYBATDAU, NGAYKETTHUC) VALUES (?, ?, ?, ?, ?)";
-        try (Connection con = DBconnect.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, u.getIdUD());
-            ps.setString(2, u.getGiaTri());
-            ps.setInt(3, u.getSoLuong());
-            ps.setDate(4, u.getNgayBatDau());
-            ps.setDate(5, u.getNgayKetThuc());
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println("Lỗi thêm ưu đãi: " + e.getMessage());
-        }
-    }
-
-    public void sua(UuDai u) {
-        String sql = "UPDATE UUDAI SET GIATRI = ?, SOLUONG = ?, NGAYBATDAU = ?, NGAYKETTHUC = ? WHERE ID_UD = ?";
-        try (Connection con = DBconnect.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, u.getGiaTri());
-            ps.setInt(2, u.getSoLuong());
-            ps.setDate(3, u.getNgayBatDau());
-            ps.setDate(4, u.getNgayKetThuc());
-            ps.setString(5, u.getIdUD());
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println("Lỗi cập nhật ưu đãi: " + e.getMessage());
-        }
-    }
-
-    public void xoa(String idUD) {
-        String sql = "DELETE FROM UUDAI WHERE ID_UD = ?";
+    public UuDai timKiem(String idUD) {
+        String sql = "SELECT * FROM UUDAI WHERE ID_UD = ?";
         try (Connection con = DBconnect.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, idUD);
-            ps.executeUpdate();
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                UuDai u = new UuDai();
+                u.setIdUD(rs.getString(1));
+                u.setGiaTri(rs.getString(2));
+                u.setSoLuong(rs.getInt(3));
+                u.setNgayBatDau(rs.getDate(4));
+                u.setNgayKetThuc(rs.getDate(5));
+                return u;
+            }
         } catch (Exception e) {
-            System.out.println("Lỗi xóa ưu đãi: " + e.getMessage());
+            System.out.println("Lỗi: " + e.getMessage());
         }
+        return null;
     }
 
-    public List<UuDai> getAll_GiaTri() {
-        List<UuDai> lstSL = new ArrayList<>();
-        String sql = "SELECT GIATRI, SOLUONG FROM UUDAI";
-        Connection con = DBconnect.getConnection();
-        try {
-            Statement stm = con.createStatement();
-            ResultSet rs = stm.executeQuery(sql);
+    public List<UuDai> locTheoGiaTri(String giaTri) {
+        List<UuDai> list = new ArrayList<>();
+        String sql = "SELECT * FROM UUDAI WHERE GIATRI = ?";
+
+        try (Connection conn = DBconnect.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, giaTri);
+            ResultSet rs = ps.executeQuery();
+
             while (rs.next()) {
                 UuDai u = new UuDai();
-                u.setGiaTri(rs.getString("GIATRI"));
-                u.setSoLuong(rs.getInt("SOLUONG"));
-                lstSL.add(u);
+                u.setIdUD(rs.getString(1));
+                u.setGiaTri(rs.getString(2));
+                u.setSoLuong(rs.getInt(3));
+                u.setNgayBatDau(rs.getDate(4));
+                u.setNgayKetThuc(rs.getDate(5));
+                list.add(u);
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return lstSL;
-    }
 
-    public Object[] getRow_GT(UuDai u) {
-        return new Object[]{u.getGiaTri(), u.getSoLuong()};
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
