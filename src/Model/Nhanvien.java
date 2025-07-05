f5ca9b3cb8c6c4ea4abebdc2070213c971362daa
baobao-2008 <baovnqth06015@gmail.com>
2025-07-05 14:48:09 +0700

    classnhanvienandtaikhoan

--- src/Model/Nhanvien.java
+++ src/Model/Nhanvien.java
@@ -9,5 +9,57 @@ package Model;
  * @author ADMIN
  */
 public class Nhanvien {
+    private String ID_NV, hoTen, SDT, chucVu, IMG;
+
+    public Nhanvien() {
+    }
+
+    public Nhanvien(String ID_NV, String hoTen, String SDT, String chucVu, String IMG) {
+        this.ID_NV = ID_NV;
+        this.hoTen = hoTen;
+        this.SDT = SDT;
+        this.chucVu = chucVu;
+        this.IMG = IMG;
+    }
+
+    public String getID_NV() {
+        return ID_NV;
+    }
+
+    public void setID_NV(String ID_NV) {
+        this.ID_NV = ID_NV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public String getIMG() {
+        return IMG;
+    }
+
+    public void setIMG(String IMG) {
+        this.IMG = IMG;
+    }
     
 }
--- src/Model/Taikhoan.java
+++ src/Model/Taikhoan.java
@@ -9,5 +9,39 @@ package Model;
  * @author ADMIN
  */
 public class Taikhoan {
+    private String ID_TK, pass, vaiTro;
+
+    public Taikhoan() {
+    }
+
+    public Taikhoan(String ID_TK, String pass, String vaiTro) {
+        this.ID_TK = ID_TK;
+        this.pass = pass;
+        this.vaiTro = vaiTro;
+    }
+
+    public String getID_TK() {
+        return ID_TK;
+    }
+
+    public void setID_TK(String ID_TK) {
+        this.ID_TK = ID_TK;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(String vaiTro) {
+        this.vaiTro = vaiTro;
+    }
     
 }
