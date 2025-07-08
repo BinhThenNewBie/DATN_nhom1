ba7df23644cf9ffbe5ca4388ae1c62b41273aaea
baobao-2008 <baovnqth06015@gmail.com>
2025-07-08 11:25:36 +0700

    getrowandgetalltaikhoan

--- src/DAO/TaikhoanDAO.java
+++ src/DAO/TaikhoanDAO.java
@@ -4,10 +4,42 @@
  */
 package DAO;
 
+import DBconnect.DBconnect;
+import Model.Taikhoan;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 /**
  *
  * @author ADMIN
  */
 public class TaikhoanDAO {
-    
+ public List<Taikhoan> GETALL(){
+        List<Taikhoan> Listtk =new ArrayList<>();
+        String sql = "SELECT * FROM TAIKHOAN" ;
+        try {
+            Connection conn = DBconnect.getConnection();
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {
+                Taikhoan tk = new Taikhoan();
+                tk.setID_TK(rs.getString(1));
+                tk.setPass(rs.getString(2));
+                tk.setVaiTro(rs.getString(3));
+                Listtk.add(tk);
+            }
+        } catch (Exception e) {
+        }
+        return Listtk;
+    }  
+    public Object[] GETROW(Taikhoan tk){
+        String ID_TK = tk.getID_TK();
+        String Pass = tk.getPass();
+        String vaiTro = tk.getVaiTro();
+        Object[] rows = new Object[]{ID_TK,Pass,vaiTro};
+        return rows;
+    }   
 }
